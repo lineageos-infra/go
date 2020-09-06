82c0a8f76b3724ae8c859dfc6de03bf08238bf8b
Tom Powell <tom@powell.io>
2020-09-05 21:32:32 -0700

    initial upload service

---
+++ pkg/auth/apikey.go
@@ -0,0 +1,18 @@
+package auth
+
+import "net/http"
+
+func ApiKey(next http.Handler, keys map[string]bool) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		key := r.Header.Get("Api-Key")
+		if key == "" {
+			http.Error(w, "invalid api key", http.StatusUnauthorized)
+			return
+		}
+		_, ok := keys[key]
+		if !ok {
+			http.Error(w, "invalid api key", http.StatusUnauthorized)
+		}
+		next.ServeHTTP(w, r)
+	})
+}
